bd5fa112ab439253246d4009e46762bc8973ca4f
Ziterz <ziady.mubaraq@gmail.com>
2023-06-22 16:42:42 +0700

    Initial Commit

---
+++ ShootAR/ShootARApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShootARApp.swift
+//  ShootAR
+//
+//  Created by Ziady Mubaraq on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ShootARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
